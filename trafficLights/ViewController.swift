f564e5ea75718b1736b013d9420951e0f009a5fe
RuslanBihun <bigun.ruslan@gmail.com>
2021-12-05 11:55:32 +0400

    1.3 Implementation of the light switch process

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -14,18 +14,48 @@ class ViewController: UIViewController {
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
     
+    var clickCounter = 1
     
     @IBOutlet var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+        yellowLight.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
+        greenLight.backgroundColor = UIColor.green.withAlphaComponent(0.3)
     }
     
     
     @IBAction func startButtonPressed(_ sender: UIButton) {
+        pushButton()
+        startButton.setTitle("NEXT", for: .normal)
     }
     
-
-}
-
+    // Light switch
+    func pushButton() {
+        
+        switch clickCounter {
+        case 1:
+            clickCounter += 1
+            redLight.backgroundColor = UIColor.red.withAlphaComponent(1)
+            yellowLight.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
+            greenLight.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+            
+        case 2:
+            clickCounter += 1
+            redLight.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+            yellowLight.backgroundColor = UIColor.yellow.withAlphaComponent(1)
+            greenLight.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+            
+        case 3:
+            clickCounter -= 2
+            redLight.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+            yellowLight.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
+            greenLight.backgroundColor = UIColor.green.withAlphaComponent(1)
+      
+        default:
+        break
+        }
+        
+    }
+    }
